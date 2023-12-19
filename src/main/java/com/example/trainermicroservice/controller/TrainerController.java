91245f1fb88a4fd3cdae8f384fc39bc177777d03
Bryan Hernandez <bryan_hernandez@epam.com>
2023-12-19 14:37:09 -0600

    Minor changes

--- src/main/java/com/example/trainermicroservice/controller/TrainerController.java
+++ src/main/java/com/example/trainermicroservice/controller/TrainerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,8 +27,9 @@ public class TrainerController {
     private TrainerServiceImpl trainerServiceImpl;
 
     @GetMapping("/workload")
-    public ResponseEntity<TrainerMonthlySummary> handleTrainerWorkload(@Valid @RequestBody TrainerMonthlySummaryRequest request) {
-        TrainerMonthlySummary response = trainerServiceImpl.calculateMonthlySummary(request);
+    public ResponseEntity<TrainerMonthlySummary> handleTrainerWorkload(@RequestParam String trainerUsername) {
+        TrainerMonthlySummaryRequest trainerMonthlySummaryRequest = new TrainerMonthlySummaryRequest(trainerUsername);
+        TrainerMonthlySummary response = trainerServiceImpl.calculateMonthlySummary(trainerMonthlySummaryRequest);
 
         HttpStatus responseStatus = response == null ? HttpStatus.BAD_REQUEST : HttpStatus.ACCEPTED;
 
